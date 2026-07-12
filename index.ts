8e7a72d73b02ef6543bafd1db03193cc33eacab0
大岡伶 <rei@MacBook-Air.local>
2026-07-12 17:36:43 +0900

    Add age field to user

--- index.ts
+++ index.ts
@@ -27,8 +27,9 @@ app.get("/", async (req, res) => {
 // ユーザーを追加する処理
 app.post("/users", async (req, res) => {
   const name = req.body.name;
+  const age = req.body.age ? Number(req.body.age) : null; // 数値に変換するのじゃ
   if (name) {
-    await prisma.user.create({ data: { name } });
+    await prisma.user.create({ data: { name, age } });
   }
   res.redirect("/");
 });
